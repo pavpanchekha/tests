561a3379fde8838dde4462028eb18d34d7da755d
Live session user <ubuntu@ubuntu.(none)>
2009-03-27 17:59:58 +0000

    Multiple tests per doc -t command line flag for different default tool

--- tests.py
+++ tests.py
@@ -4,69 +4,87 @@ import sys, os
 import subprocess
 
 class Test(object):
-    def __init__(self, input="", output="", desc="", tool="python"):
+    def __init__(self, tests=None, desc="", tool="python"):
+        if not tests: tests = [["", ""]] # Stupid evaluate-once arguments
         self.desc = desc
-        self.input = input
-        self.output = output
+        self.tests = tests
         self.tool = tool
 
     def trim(self):
         self.desc = self.desc.strip()
-        self.input = self.input.strip()
-        self.output = self.output.strip()
+        
+        if self.tests[0] == ["", ""]: del self.tests[0]
+        
+        for i in self.tests:
+            i[0] = i[0].strip()
+            i[1] = i[1].strip()
+            
         self.tool = self.tool.strip()
 
     def run(self, quiet=False):
-        if not self.input: return
-        cmd = subprocess.Popen(self.tool.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = cmd.communicate(self.input)
+        if not self.tests: return 0
+        failed = []
 
-        if out.strip() == self.output:
-            if not quiet:
-                print ".", self.desc
+        for i in self.tests:
+            cmd = subprocess.Popen(self.tool.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = cmd.communicate(i[0])
             
-            return True
+            if out.strip() != i[1]:
+                failed.append((i[0], i[1], out.strip(), err.strip()))
+
+        if quiet:
+            return len(failed)
+        
+        if not failed:
+            print ".", self.desc
+            return 0
         else:
-            if not quiet:
-                print "F", self.desc
+            print "F", self.desc, "(" + str(len(failed)) + "/" + str(len(self.tests)) + ")"
+            for i in failed:
+                print "===== Input: ======"
+                print i[0]
+                print "===== Expected: ==="
+                print i[1]
                 print "===== Output: ====="
-                print out.strip()
+                print i[2]
                 print "===== Error: ======"
-                print err.strip()
+                print i[3]
                 print "==================="
-            
-            return False
+                
+            return len(failed)
 
-def parsetests(strtests):
+def parsetests(strtests, deftool="python"):
     tests = []
-    curr = Test()
+    curr = Test(tool=deftool)
     
     for s in strtests.split("\n"):
         if len(s) == 0: continue
 
-        if s[0] == "#" and s[1] != " ":
-            s = s[1:]
-            if s[:1] == "!":
-                tests.append(curr)
-                curr = Test(tool=s[1:].strip())
-            elif s[:1] == "#":
-                continue
+        if s[0] == "#" and s[1] == "!":
+            s = s[2:]
+            tests.append(curr)
+            curr = Test(tool=s.strip())
+        elif s[:2] == "##":
+            continue
         elif s[:2] == "# ":
-            if curr.input:
+            if curr.tests[-1][1]:
                 tests.append(curr)
                 curr = Test(desc=s[2:].strip(), tool=curr.tool)
             else:
                 curr.desc += "\n" + s[1:].strip()
         elif s[:4] == ">>> ":
-            if curr.output:
-                tests.append(curr)
-                curr = Test(input=s[4:], tool=curr.tool)
+            if curr.tests[-1][1]:
+                curr.tests.append([s[4:], ""])
             else:
-                curr.input += "\n" + s[4:]
+                curr.tests[-1][0] += "\n" + s[4:]
         else:
-            curr.output += "\n" + s
+            curr.tests[-1][1] += "\n" + s
 
     tests.append(curr)
+    
+    if tests[0].tests[0] == ["", ""]:
+        tests = tests[1:]
+    
     return tests
 
 def run(l, quiet=False):
@@ -77,23 +95,20 @@ def run(l, quiet=False):
         test.trim()
         
         tot += 1
-        if not test.run(quiet): failed += 1
-
-    tot -= 1
-    failed -= 1
+        failed += test.run(quiet)
 
     if not quiet:
         print "Failed %d out of %d" % (failed, tot)
 
     return (tot, failed)
 
-def run_file(f, quiet=False):
+def run_file(f, quiet=False, deftool="python"):
     if os.path.isdir(f):
         t = []
         tot = 0
         os.chdir(f)
-        for i in filter(os.path.isfile, os.listdir(".")):
-            tt, r = run_file(i, True)
+        for i in [i for i in os.listdir(".") if os.path.isfile(i) and not i.endswith("~") and not i.startswith(".")]:
+            tt, r = run_file(i, True, deftool)
             tot += tt
             if r > 0:
                 t.append((i, r))
@@ -111,15 +126,25 @@ def run_file(f, quiet=False):
         return t
     else:
         f = open(f)
-        return run(parsetests(f.read()), quiet)
+        return run(parsetests(f.read(), deftool), quiet)
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
+    args = sys.argv
+
+    if len(args) == 1:
         try:
             while 1:
                 run_file(raw_input("Tests File? "))
         except (KeyboardInterrupt, EOFError):
             pass
     else:
+        if "-t" in args:
+            x = args.index("-t")
+            if x+1 < len(args):
+                tool = args[x + 1]
+                del args[x:x + 2]
+        else:
+            tool = "python"
+        
         for i in sys.argv[1:]:
-            run_file(i)
+            run_file(i, deftool=tool)
